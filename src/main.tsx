2b73800b04d28bea6183bb028af10c0507214dc2
Aditya Karki <adityakarki201@gmail.com>
2025-03-26 16:21:01 +0530

    Add Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import AllNews from "./pages/AllNews";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<AllNews />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
