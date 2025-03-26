100a3378d289975032cd3d9a4fdc30b7196e9c68
Aditya Karki <adityakarki201@gmail.com>
2025-03-26 17:24:58 +0530

    Feat: Add navigation using react router

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { Flex, Segmented } from "antd";
+import { useNavigate } from "react-router";
+
+const Navbar = () => {
+  //   const newsOptions = ["Technology", "Sports", "Business"];
+  const navigate = useNavigate();
+  const newsOptions = [
+    {
+      label: "Technology",
+      value: "/technology",
+    },
+    {
+      label: "Sports",
+      value: "/sports",
+    },
+    {
+      label: "Business",
+      value: "/business",
+    },
+  ];
+
+  return (
+    <div className="sticky border border-black/10 min-w-lg w-screen  py-2 shadow-md flex justify-center ">
+      <Flex gap="middle" align="center">
+        <Segmented
+          options={newsOptions}
+          size="large"
+          shape="round"
+          onChange={(value) => navigate(value)}
+        />
+      </Flex>
+    </div>
+  );
+};
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,37 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import AllNews from "./pages/AllNews";
+import AllTechNews from "./pages/AllTechNews";
+import AllBusinessNews from "./pages/AllBusinessNews";
+import AllSportsNews from "./pages/AllSportsNews";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />}>
-          <Route index element={<AllNews />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        components: {
+          Segmented: {
+            /* here is your component tokens */
+            // itemActiveBg: "#2d57dd",
+            itemSelectedBg: "#2563EB",
+            itemSelectedColor: "#ffffff",
+            // itemHoverColor: "#ffffff",
+            // itemHoverBg: "#2563EB",
+          },
+        },
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />}>
+            <Route index element={<AllNews />} />
+            <Route path="/sports" element={<AllSportsNews />} />
+            <Route path="/technology" element={<AllTechNews />} />
+            <Route path="/business" element={<AllBusinessNews />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   </StrictMode>
 );
