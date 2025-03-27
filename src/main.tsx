94b4fc25d9a1fea2564501c272cd30b5f0b64031
Aditya Karki <adityakarki201@gmail.com>
2025-03-27 12:13:32 +0530

    Feat: Provide store to all react app

--- src/main.tsx
+++ src/main.tsx
@@ -6,35 +6,39 @@ import Dashboard from "./pages/Dashboard";
 import AllNews from "./pages/AllNews";
 import AllTechNews from "./pages/AllTechNews";
 import AllBusinessNews from "./pages/AllBusinessNews";
-import AllSportsNews from "./pages/AllSportsNews";
+import AllSportsNews from "./pages/AllSports/AllSportsNews";
 import { ConfigProvider } from "antd";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider
-      theme={{
-        components: {
-          Segmented: {
-            /* here is your component tokens */
-            // itemActiveBg: "#2d57dd",
-            itemSelectedBg: "#2563EB",
-            itemSelectedColor: "#ffffff",
-            // itemHoverColor: "#ffffff",
-            // itemHoverBg: "#2563EB",
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          components: {
+            Segmented: {
+              /* here is your component tokens */
+              // itemActiveBg: "#2d57dd",
+              itemSelectedBg: "#2563EB",
+              itemSelectedColor: "#ffffff",
+              // itemHoverColor: "#ffffff",
+              // itemHoverBg: "#2563EB",
+            },
           },
-        },
-      }}
-    >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard />}>
-            <Route index element={<AllNews />} />
-            <Route path="/sports" element={<AllSportsNews />} />
-            <Route path="/technology" element={<AllTechNews />} />
-            <Route path="/business" element={<AllBusinessNews />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ConfigProvider>
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard />}>
+              <Route index element={<AllNews />} />
+              <Route path="/sports" element={<AllSportsNews />} />
+              <Route path="/technology" element={<AllTechNews />} />
+              <Route path="/business" element={<AllBusinessNews />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
+    </Provider>
   </StrictMode>
 );
